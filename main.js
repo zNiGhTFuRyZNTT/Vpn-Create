a001568236c091bd7097a80612bde6c4277079b9
NF <78607877+zNiGhTFuRyZNTT@users.noreply.github.com>
2022-09-21 13:22:05 +1000

    add cluster to handle unexpected errors

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const TeleBot = require('telebot')
+const cluster = require('cluster')
 const { exec } = require("child_process")
-var AdmZip = require("adm-zip");
+var AdmZip = require("adm-zip")
 const { create_vpn, random_str, throw_key_err, revoke_user, list_users, create_zip, random_int } = require("./functions")
 require('dotenv').config()
 const token = process.env.API_KEY
-const bot = new TeleBot(token)
+const bot = new TeleBot({
+    token: token,
+    usePlugins: ['floodProtection',]
+})
 const admins = process.env.ADMINS.split(",").map(Number)
 const UNIQUE_KEY = process.env.UNIQUE_KEY
 const keys = [UNIQUE_KEY, ]
@@ -17,13 +21,18 @@ bot.on(/^\/createkey (.+)$/, async (msg, props) => {
     const key = (random_str() + "0" + name + "1" + random_str()).toUpperCase()
     console.log(`[>] Key created -> ${key}`);
     keys.push(key)
-    await msg.reply.text(`[>] Key created!\n\tkeyword=${name}\n\tKey= ${key}`).catch(console.log)
+    msg.reply.text(`[>] Key created!\n\tKeyword=${name}`).then(() => {
+        msg.reply.text(`${key}`).catch(msg.reply.text("Error happened please contact admin as soon as possible @NiGhTFuRyZz").catch(console.log))
+
+    }).catch(() => {
+        msg.reply.text("Error happened please contact admin as soon as possible @NiGhTFuRyZz").catch(console.log)
+    })
 
 })
 
 
 bot.on('/keys', async (msg) => {
-    await bot.sendMessage(msg.chat.id, `${keys}`).catch(console.log)
+    bot.sendMessage(msg.chat.id, `${keys}`).catch(console.log)
     return
 })
 
@@ -43,7 +52,7 @@ bot.on(/^\/vpn (.+)$/, async (msg, props) => {
     }
     const is_unique = req.key == UNIQUE_KEY 
     console.log(req)
-    if (keys.includes(req.key)) {
+    if (keys.includes(req.key) || is_unique) {
 
         console.log(keys.includes(req.key) || [UNIQUE_KEY].includes(req.key));
         const prepMsg = await msg.reply.text("[🍑] Creating vpn please wait ...")
@@ -52,11 +61,11 @@ bot.on(/^\/vpn (.+)$/, async (msg, props) => {
         const path = `storage/${filename}.ovpn`
         console.log(filename);
         !is_unique ? keys.splice(req.key, 1) : {}
-        await create_vpn(keys, req.key, `${filename}`).catch(err => console.log(err.message))
+        await create_vpn(keys, req.key, `${filename}`)
         await bot.editMessageText({chatId: msg.chat.id, messageId: msgId}, '[🔞] Vpn created, sending ...').catch(err => console.log(err))
-        await create_zip(path, filename).catch(err => console.log(err.message))
-        await bot.sendDocument(msg.chat.id, `./storage/${filename}.zip`).catch(err => console.log(err.message))
-        await bot.deleteMessage(msg.chat.id, msgId).catch(err => msg.reply.text(err.message))
+        await create_zip(path, filename).catch(err => console.log(err))
+        await bot.sendDocument(msg.chat.id, `./storage/${filename}.zip`).catch(err => console.log(err))
+        await bot.deleteMessage(msg.chat.id, msgId).catch(err => msg.reply.text(err))
         return
     }
     msg.reply.text("[❗] Key not valid, please contact @NiGhTFuRyZz")
@@ -72,10 +81,21 @@ bot.on(/^\/revoke (.+)$/, async (msg, props) => {
 bot.on('/list', async (msg) => {
     const is_admin = (admins.indexOf(msg.from.id) >= 0)
     if (!is_admin) return
-    const users_list = await list_users(is_admin).catch(console.log)
-    await msg.reply.text(users_list).catch(err => console.log(err))
+    list_users(is_admin)
+        .then(users_list=> {
+            msg.reply.text(users_list).catch(err => console.log(err))
+        })
+        .catch(console.log)
 })
 
 
+if (cluster.isMaster) {
+    cluster.fork()
 
-bot.start()
+    cluster.on('exit', (worker, code, signal) => {
+        cluster.fork()
+    })
+}
+if (cluster.isWorker) {
+    bot.start()
+}
